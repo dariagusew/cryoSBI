18be43af21ba3a6282cf851ed6be2c25399866b0
zvh378 <zvh378@sbinlabgdpx01fl.unicph.domain>
2025-12-08 17:06:31 +0100

    added example training and test script

---
+++ tutorials/run_inference_test_script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+echo "=== Started inference 10k ==="
+
+infer_populations \
+    --models_file ../../hsp90_models.pt \
+    --train_config_file ../training_parameters_nle.json \
+    --image_config_file simulation_parameters_ensemble.json \
+    --estimator_file ../tutorial_estimator \
+    --device cuda:0 \
+    --population_steps 11 \
+    --num_sim 10000 \
+    --verbose \
+    --output_prefix 10k 
+
+echo "=== Finished inference 10k ==="
+
+echo "=== Started inference 100k ==="
+
+infer_populations \
+    --models_file ../../hsp90_models.pt \
+    --train_config_file ../training_parameters_nle.json \
+    --image_config_file simulation_parameters_ensemble.json \
+    --estimator_file ../tutorial_estimator \
+    --device cuda:0 \
+    --population_steps 11 \
+    --num_sim 100000 \
+    --verbose \
+    --output_prefix 100k 
+
+echo "=== Finished inference 100k ==="
+
+echo "=== Started inference 1M ==="
+
+infer_populations \
+    --models_file ../../hsp90_models.pt \
+    --train_config_file ../training_parameters_nle.json \
+    --image_config_file simulation_parameters_ensemble.json \
+    --estimator_file ../tutorial_estimator \
+    --device cuda:0 \
+    --population_steps 11 \
+    --num_sim 1000000 \
+    --verbose \
+    --output_prefix 1M \
+    --batch_size 100 #only for generating 1M cryo images, otherwise memory error
+
+echo "=== Finished inference 1M ==="

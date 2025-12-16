f3e32dc626ffb206682a49ce8706a2d1ba679168
zvh378 <zvh378@sbinlabgdpx03fl.unicph.domain>
2025-12-16 23:52:19 +0100

    light weight benchmark

---
+++ tutorials/HSP90_bechmark_0_light/SPATIAL_CRYO/run_training.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+# Step 1: Pretrain SPATIAL_CRYO
+echo "=== Started pretraining SPATIAL_CRYO ==="
+
+pretrain_spatial_cryo \
+    --embedding SPATIAL_CRYO \
+    --epochs 100 \
+    --batch_size 256 \
+    --device cuda:0 \
+    --output pretrained_spatial_cryo.pt \
+    --embedding_dim 256 \
+    --image_config simulation_parameters.json \
+    --lr 0.0002 \
+    --simulation_batch_size 1024
+
+echo "=== Finished pretraining SPATIAL_CRYO ==="
+
+
+# Step 2: Train NLE model 
+echo "=== Started training NLE ==="
+
+train_nle_model \
+    --image_config_file simulation_parameters.json \
+    --train_config_file training_parameters_nle.json\
+    --epochs 200 \
+    --estimator_file tutorial_estimator \
+    --loss_file tutorial.loss \
+    --n_workers 4 \
+    --train_device cuda:0 \
+    --pretrained_embedding_path pretrained_spatial_cryo.pt \
+    --freeze_embedding True \
+    --saving_freq 100 \
+    --simulation_batch_size 2048 \
+    
+echo "=== Finished training NLE ==="

e1baf8157aa51dd0a50d6f8b4f2d35ca1688a57d
Massimiliano Bonomi <mbonomi@oviedo.bis.pasteur.fr>
2025-12-31 14:54:56 +0100

    check positive

--- src/cryo_sbi/wpa_simulator/noise.py
+++ src/cryo_sbi/wpa_simulator/noise.py
@@ -120,7 +120,14 @@ def add_Poisson_noise(
     # 6. Determine the mean electron count per pixel
     mean_counts_per_pixel = qe * mean_electron_dose * (1.0 + contrast_scale * image)
 
-    # 7. Generate Poisson shot noise. Clamp mean counts to be non-negative.
+    # 7. Generate Poisson shot noise
+    ## First sanitize + clamp mean_counts_per_pixel to be non-negative.
+    mean_counts_per_pixel = torch.nan_to_num(
+        mean_counts_per_pixel,
+        nan=0.0,
+        posinf=torch.finfo(mean_counts_per_pixel.dtype).max,  # Use max value for the dtype
+        neginf=0.0
+    )
     mean_counts_per_pixel = torch.clamp(mean_counts_per_pixel, min=0)
     image_noise = torch.poisson(mean_counts_per_pixel)
 

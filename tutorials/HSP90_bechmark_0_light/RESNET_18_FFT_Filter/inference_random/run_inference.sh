c917b4cc39c471e75a4ff1ff98719af7cc59f50b
zvh378 <zvh378@sbinlabgdpx01fl.unicph.domain>
2026-01-06 13:40:41 +0100

    added testing light benchmark

---
+++ tutorials/HSP90_bechmark_0_light/RESNET_18_FFT_Filter/inference_random/run_inference.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+
+echo "=== Started inference 10k ==="
+
+infer_populations \
+    --models_file ../../hsp90_models-small.pt \
+    --train_config_file ../training_parameters_nle.json \
+    --image_config_file simulation_parameters_ensemble.json \
+    --estimator_file ../tutorial_estimator \
+    --device cuda:1 \
+    --population_steps 5 \
+    --num_sim 10000 \
+    --verbose \
+    --output_prefix 10k \
+    --use_random
+
+echo "=== Finished inference 10k ==="
+
+echo "=== Started inference 100k ==="
+
+infer_populations \
+   --models_file ../../hsp90_models-small.pt \
+   --train_config_file ../training_parameters_nle.json \
+   --image_config_file simulation_parameters_ensemble.json \
+   --estimator_file ../tutorial_estimator \
+   --device cuda:1 \
+   --population_steps 5 \
+   --num_sim 100000 \
+   --verbose \
+   --output_prefix 100k \
+   --use_random 
+
+echo "=== Finished inference 100k ==="
+
+echo "=== Started inference 1M ==="
+
+infer_populations \
+    --models_file ../../hsp90_models-small.pt \
+    --train_config_file ../training_parameters_nle.json \
+    --image_config_file simulation_parameters_ensemble.json \
+    --estimator_file ../tutorial_estimator \
+    --device cuda:1 \
+    --population_steps 5 \
+    --num_sim 1000000 \
+    --verbose \
+    --output_prefix 1M \
+    --use_random 
+
+echo "=== Finished inference 1M ==="

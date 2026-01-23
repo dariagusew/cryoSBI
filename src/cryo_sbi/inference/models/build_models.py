106f690f2d725c24abe404cf794e158f59764502
zvh378 <zvh378@sbinlabgdpx05fl.unicph.domain>
2026-01-23 13:06:42 +0100

    add output_dim_x and output_dim_theta in NLE estimator

--- src/cryo_sbi/inference/models/build_models.py
+++ src/cryo_sbi/inference/models/build_models.py
@@ -81,8 +81,11 @@ def build_nle_flow_model(config: dict, image_size: int, **embedding_kwargs) -> n
         )
 
     try:
-        embedding = partial(
-            EMBEDDING_NETS[config["EMBEDDING"]], config["OUT_DIM"], image_size, **embedding_kwargs
+        embedding_x = partial(
+            EMBEDDING_NETS[config["EMBEDDING"]], config["OUT_DIM_X"], image_size, **embedding_kwargs
+        )
+        embedding_theta = partial(
+            EMBEDDING_NETS[config["EMBEDDING"]], config["OUT_DIM_THETA"], image_size, **embedding_kwargs
         )
     except KeyError:
         raise NotImplementedError(
@@ -91,8 +94,10 @@ The following embeddings are implemented : {[key for key in EMBEDDING_NETS.keys(
         )
 
     estimator = estimator_models.NLEWithEmbedding(
-        embedding_net=embedding,
-        output_embedding_dim=config["OUT_DIM"],
+        embedding_net_x=embedding_x,
+        embedding_net_theta=embedding_theta,
+        output_embedding_dim_x=config["OUT_DIM_X"],
+        output_embedding_dim_theta=config["OUT_DIM_THETA"],
         num_transforms=config["NUM_TRANSFORM"],
         num_hidden_flow=config["NUM_HIDDEN_FLOW"],
         hidden_flow_dim=config["HIDDEN_DIM_FLOW"],
